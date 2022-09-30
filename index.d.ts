3e7bba81ac22d22b41bde5f9aba5918626652cab
Wald Habets <wald.habets@protonmail.com>
2022-09-30 16:53:24 +0200

    component exports

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare namespace _default {
+    export { PageLayout };
+    export { SidebarGroup };
+    export { SidebarTab };
+}
+export default _default;
+import PageLayout from "./page-layout.svelte";
+import SidebarGroup from "./sidebar-group.svelte";
+import SidebarTab from "./sidebar-tab.svelte";
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Reexport your entry components here
+import PageLayout from "./page-layout.svelte";
+import SidebarGroup from "./sidebar-group.svelte";
+import SidebarTab from "./sidebar-tab.svelte";
+export default { PageLayout, SidebarGroup, SidebarTab };
