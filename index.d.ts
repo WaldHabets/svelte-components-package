a39f4ad0031ce73f058df3c66ee648440f66f78d
Wald Habets <wald.habets@protonmail.com>
2022-10-14 18:56:06 +0200

    updated exports

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,20 @@
+/// <reference types="svelte/types/runtime/ambient" />
 declare namespace _default {
     export { PageLayout };
     export { SidebarTab };
+    export { DetailsContainer };
+    export { ListItemActive };
+    export { ListItemLink };
+    export { ModalAction };
+    export { ModalAlert };
+    export { ModalPicker };
 }
 export default _default;
 import PageLayout from "./page-layout.svelte";
 import SidebarTab from "./sidebar-tab.svelte";
+import DetailsContainer from "./details-container.svelte";
+import ListItemActive from "./list-item-active.svelte";
+import ListItemLink from "./list-item-link.svelte";
+import ModalAction from "./modal-action.svelte";
+import ModalAlert from "*.svelte";
+import ModalPicker from "*.svelte";
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Reexport your entry components here
 import PageLayout from "./page-layout.svelte";
 import SidebarTab from "./sidebar-tab.svelte";
-export default { PageLayout, SidebarTab };
+import DetailsContainer from "./details-container.svelte";
+import ListItemActive from "./list-item-active.svelte";
+import ListItemLink from "./list-item-link.svelte";
+import ModalAction from "./modal-action.svelte";
+import ModalAlert from "svelte-components/modal-alert.svelte";
+import ModalPicker from "svelte-components/modal-picker.svelte";
+export default { PageLayout, SidebarTab, DetailsContainer, ListItemActive, ListItemLink, ModalAction, ModalAlert, ModalPicker };
